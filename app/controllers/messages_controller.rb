50ed017f06121509e5ef539bbd77af2250fe7cf7
sirSlappz <tamestranger43@gmail.com>
2022-07-10 12:08:21 -0500

    copied Cendeu commit 'fully impliment game chat (for guests and users)'

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,36 +1,38 @@
 class Api::V1::MessagesController < ApplicationController
 
-    before_action :authenticate_user!, except: :send_game_message
+  before_action :authenticate_user!, except: :send_game_message
 
-    def create
-        message = current_user.messages.build(message_params)
-        message.save!
-    end
+  def create
+      message = current_user.messages.build(message_params)
+      message.save!
+  end
 
-    def send_game_message
-      game = Game.find(message_params[:game_id])
-      if user_signed_in?
-        username = current_user.email.split('@')[0] 
+  def send_game_message
+    game = Game.find(message_params[:game_id])
+    if user_signed_in?
+      username = current_user.email.split('@')[0] 
+    elsif message_params[:token]
+      token = Token.find_by token: message_params[:token]
+      if token
+        user = token.guest_user
+        username = "Guest_#{user.id}"  
       end
-      if message_params[:token] != nil
-        user = Token.find_by(token, message_params[:token]).guest_user
-        username = "Guest_#{guest_user.id}"  
-      end
-      message = {user: username, body: message_params[:body]}
-      broadcast_to(game, {type: "game_message", message: message})
     end
-    
-    def send_lobby_message
-      user = current_user if user_signed_in?
-      username = user.email.split('@')[0]
-      complete_message = {user: username, body: message_params[:message]}
-      ActionCable.server.broadcast("lobby", {type: "lobby_message", message: complete_message})
-    end
+    message = {user: username, body: message_params[:message]}
+    GameChannel.broadcast_to(game, {type: "chat_message", message: message})
+  end
+  
+  def send_lobby_message
+    user = current_user if user_signed_in?
+    username = user.email.split('@')[0]
+    complete_message = {user: username, body: message_params[:message]}
+    ActionCable.server.broadcast("lobby", {type: "lobby_message", message: complete_message})
+  end
 
-    private
+  private
 
-    def message_params
-        params.require(:message).permit(:body, :token, :game_id, :message)
-    end
+  def message_params
+      params.permit(:body, :token, :game_id, :message)
+  end
 
 end
